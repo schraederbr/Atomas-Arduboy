41c387d26f2b2c818209dd6ae1b0b72fa74f5f6e
Ben Schraeder <schraeder.br@gmail.com>
2023-05-11 13:00:45 -0600

    Added calculation files and .vscode folder

---
+++ calculations.py
@@ -0,0 +1,18 @@
+from math import radians, sin
+from math import cos
+from math import tan
+from math import pi
+from math import sqrt
+from math import atan
+from math import asin
+from math import acos
+from math import degrees
+
+radius = 24
+
+# Convert degrees to radians
+print(radians(45))
+
+
+
+
